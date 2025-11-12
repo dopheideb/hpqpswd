e4d44d1773365b20f6f37299e0b33e59d0cc67f1
Christian Sarre <christian@sarre.eu>
2025-11-12 20:47:00 +0200

    Added the possibility to encrypt one's own password.
    Size marker inside the file doesn't update correctly yet, so this
    produces an identical file compared to HP's own Windows-only tools,
    only if the password is up-to 4 characters.

--- hpqpwd.sh
+++ hpqpwd.sh
@@ -11,72 +11,56 @@ CIPHER='aes-256-cbc'
 KEY='4a14b69632ff836b4288da79a549ed9d1c0bd377839fe2d65254710c3ebd1e33'
 IV='00000000000000000000000000000000'
 COMMENT=''
-ENCRYPTEDFILE="${1:-}"
+TARGETFILE="password.bin"
+decrypt="true"
+encryptedpassword=""
+args=$(getopt -n "$0" -o + -l 'decrypt,encrypt' -- "$@")
 
+eval "set -- $args"
+while [ $# -ne 0 ]; do
+  case "$1" in
+    --decrypt|-d)    decrypt="true";   shift ;;
+    --encrypt|-e)    decrypt="false";  shift ;;
+    --) shift; break ;;
+    *)  break ;;
+  esac
+done
 
-
-## Print usage if necessary.
-if [\
-	"${ENCRYPTEDFILE}" = ''\
-	-o\
-	"${ENCRYPTEDFILE}" = '--help'\
-	-o\
-	"${ENCRYPTEDFILE}" = '-h' \
-]
-then
-	cat <<-__EOT__
-Usage:
-    $0 file-encrypted-with-hpqpswd-file.bin
-
-The file should be encrypted with the HPQPswd.exe utility.
-	__EOT__
-	exit 0
+if [ "$@" ]; then
+  TARGETFILE="$@"
 fi
 
+if [ ${decrypt} = true ]; then
+  ## Check the magic value at the start of the encrypted file.
+  MAGIC_VALUE_IN_FILE="$(
+  dd\
+    if="${TARGETFILE}"\
+    bs=1\
+    count=8\
+    status=none
+  )"
+  if [ "${MAGIC_VALUE}" != "${MAGIC_VALUE_IN_FILE}" ]
+    then
+      echo "Unexpected magic value. Expected '${MAGIC_VALUE}', but got '${MAGIC_VALUE_IN_FILE}'." 1>&2
+      exit 1
+    fi
 
-
-## Check the magic value at the start of the encrypted file.
-MAGIC_VALUE_IN_FILE="$(
-	dd\
-		if="${ENCRYPTEDFILE}"\
-		bs=1\
-		count=8\
-		status=none
-)"
-if [ "${MAGIC_VALUE}" != "${MAGIC_VALUE_IN_FILE}" ]
-then
-	echo "Unexpected magic value. Expected '${MAGIC_VALUE}', but got '${MAGIC_VALUE_IN_FILE}'." 1>&2
-	exit 1
+  ## Decrypt the encrypted file.
+  dd\
+    if="${TARGETFILE}"\
+    ${COMMENT:+ skip '_HPPW12_' (8 bytes)}\
+    ${COMMENT:+ skip length of encrypted string (2 bytes)}\
+    skip=10\
+    bs=1\
+    status=none\
+    | openssl enc -d -"${CIPHER}" -K "${KEY}" -iv "${IV}"
+  else
+    encryptedpassword=$(IFS=$'\n' read -r -p "Input password: " password 
+    echo -n "$password" > "/tmp/rawpassword.tmp"
+    echo -n "$password" | openssl enc -"${CIPHER}" -K "${KEY}" -iv "${IV}")
+    passwordsize=${#encryptedpassword}
+    hexsize=$(printf "%02X" "$passwordsize")
+    printf "${MAGIC_VALUE}\x$hexsize\x00" > "${TARGETFILE}"
+    $(iconv -f UTF-8 -t UTF-16LE "/tmp/rawpassword.tmp"| openssl enc -"${CIPHER}" -K "${KEY}" -iv "${IV}" >> "${TARGETFILE}")
+    rm "/tmp/rawpassword.tmp"
 fi
-
-
-
-## Decrypt the encrypted file.
-dd\
-	if="${ENCRYPTEDFILE}"\
-	\
-	${COMMENT:+ skip '_HPPW12_' (8 bytes)}\
-	${COMMENT:+ skip length of encrypted string (2 bytes)}\
-	skip=10\
-	bs=1\
-	status=none\
-| openssl\
-	enc\
-	\
-	${COMMMENT:+ '-d' means decrypt}\
-	-d\
-	-"${CIPHER}"\
-	-K "${KEY}"\
-	-iv "${IV}"
-OPENSSL_EXITCODE="${PIPESTATUS[1]}"
-
-
-
-if tty --silent
-then
-	## We are connected to a terminal, so add a newline for 
-	## readability.
-	echo
-fi
-
-exit "${OPENSSL_EXITCODE}"
