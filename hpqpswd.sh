5b109ebf19fa6737609de78fe06358d5f8bb8744
Christian Sarre <christian@sarre.eu>
2025-11-21 21:40:13 +0200

    Add comments to code

--- hpqpswd.sh
+++ hpqpswd.sh
@@ -54,9 +54,12 @@ if [ ${decrypt} = true ]; then
     status=none\
     | openssl enc -d -"${CIPHER}" -K "${KEY}" -iv "${IV}"
 else
+  # Read password from terminal input and encode it in UTF-16LE
   u16encpassword=$(IFS=$'\n' read -r -n 32 -p "Input password: " password 
   echo -n $password | iconv -f UTF-8 -t UTF-16LE | openssl enc -"${CIPHER}" -K "${KEY}" -iv "${IV}")
+  # Get password size from the UTF-16LE encoded string
   passwordsize=${#u16encpassword}
+  # Round passwordsize value to the next multiple of 16, to a maximum size of 80 (32 characters)
   if [ $passwordsize -le 16 ]; then
     passwordsize=16
   elif [ $passwordsize -gt 16 ] && [ $passwordsize -lt 32 ]; then
@@ -68,7 +71,10 @@ else
   elif [ $passwordsize -gt 64 ] && [ $passwordsize -lt 80 ]; then
     passwordsize=80
   fi
+  # "Convert" passwordsize to hex presentation
   hexsize=$(printf "%02X" "$passwordsize")
+  # Print {MAGIC_VALUE}, {hexsize} and x00 to target file
   printf "${MAGIC_VALUE}\x$hexsize\x00" > "${TARGETFILE}"
+  # Append the UTF-16LE encoded password to target file (must be done with echo)
   echo -n "${u16encpassword}" >> "${TARGETFILE}"
 fi
